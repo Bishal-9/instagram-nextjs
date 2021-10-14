60062cb362349b41885e6c9363b4050ba2dabc6d
Bishal9 <goonbishal.bg@gmail.com>
2021-10-14 14:17:44 +0530

    Posts

--- components/Posts.js
+++ components/Posts.js
@@ -1,7 +1,44 @@
+import Post from "./Post"
+
+const dummy_data = [
+    {
+        id: 12,
+        username: 'everef',
+        userImg: 'https://www.nicepng.com/png/full/780-7805650_generic-user-image-male-man-cartoon-no-eyes.png',
+        img: 'https://adscode.com/wp-content/uploads/2018/08/Blog-Post-Social-Media-Marketing1-1.png',
+        caption: 'Kuch bhivdsvsdsvsdvsdvsdvsdvsdzxccbfdnnnyhfg,yhf'
+    },
+    {
+        id: 12,
+        username: 'everef',
+        userImg: 'https://www.nicepng.com/png/full/780-7805650_generic-user-image-male-man-cartoon-no-eyes.png',
+        img: 'https://adscode.com/wp-content/uploads/2018/08/Blog-Post-Social-Media-Marketing1-1.png',
+        caption: 'Kuch bhi'
+    },
+    {
+        id: 12,
+        username: 'everef',
+        userImg: 'https://www.nicepng.com/png/full/780-7805650_generic-user-image-male-man-cartoon-no-eyes.png',
+        img: 'https://adscode.com/wp-content/uploads/2018/08/Blog-Post-Social-Media-Marketing1-1.png',
+        caption: 'Kuch bhi'
+    }
+]
+
 function Posts() {
     return (
         <div>
-            
+            {
+                dummy_data.map(post => (
+                    <Post
+                        key={post.id}
+                        id={post.id}
+                        username={post.username}
+                        userImg={post.userImg}
+                        img={post.img}
+                        caption={post.caption}
+                    />
+                ))
+            }
         </div>
     )
 }
